7eda43f681541584bc4c5db26736d13646ba7651
Gareth Tan <garethwtan@gmail.com>
2014-03-06 16:03:43 -0500

    Add styles.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('sgmobileApp')
-  .controller('ExploreController', function ($scope) {
+  .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
