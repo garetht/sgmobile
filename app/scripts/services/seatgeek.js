988c39d183f6102b21010aff99db8593c2679367
Gareth Tan <garethwtan@gmail.com>
2014-03-06 14:46:50 -0500

    Rename SCSS file.

--- app/scripts/services/seatgeek.js
+++ app/scripts/services/seatgeek.js
@@ -2,13 +2,14 @@
 
 angular.module('sgmobileApp')
   .service('Seatgeek', function Seatgeek($resource) {
-    this.Events = $resource('http://api.seatgeek.com/2/events', {'postal_code': 10003}, {
+    var API_URL = 'http://api.seatgeek.com/2';
+    this.Events = $resource(API_URL + '/events', {}, {
       'getEvents': {method: 'get', isArray: false, params: {'postal_code': 10003}}
     });
-    this.Performers = $resource('http://api.seatgeek.com/2/performers', {'postal_code': 10003}, {
+    this.Performers = $resource(API_URL + '/performers', {}, {
       'getPerformers': {method: 'get', isArray: false, params: {'postal_code': 10003}}
     });
-    this.Venues = $resource('http://api.seatgeek.com/2/venues', {'postal_code': 10003}, {
+    this.Venues = $resource(API_URL + '/venues', {}, {
       'getVenues': {method: 'get', isArray: false, params:{'postal_code': 10003}}
     });
   });
