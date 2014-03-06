de18c6161bc9f4ed4af7619ee2897ccfe154b9b4
Gareth Tan <garethwtan@gmail.com>
2014-03-06 14:37:44 -0500

    Seatgeek service.

--- app/scripts/services/seatgeek.js
+++ app/scripts/services/seatgeek.js
@@ -3,9 +3,12 @@
 angular.module('sgmobileApp')
   .service('Seatgeek', function Seatgeek($resource) {
     this.Events = $resource('http://api.seatgeek.com/2/events', {'postal_code': 10003}, {
-      'getEvents': {method: 'get', isArray: false}
+      'getEvents': {method: 'get', isArray: false, params: {'postal_code': 10003}}
     });
     this.Performers = $resource('http://api.seatgeek.com/2/performers', {'postal_code': 10003}, {
-      'getPerformers': {method: 'get', isArray: false}
+      'getPerformers': {method: 'get', isArray: false, params: {'postal_code': 10003}}
+    });
+    this.Venues = $resource('http://api.seatgeek.com/2/venues', {'postal_code': 10003}, {
+      'getVenues': {method: 'get', isArray: false, params:{'postal_code': 10003}}
     });
   });
