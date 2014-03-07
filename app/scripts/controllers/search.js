e3a55ad824a104f1a0477f9ffc027fb25bb8f864
Gareth Tan <garethwtan@gmail.com>
2014-03-07 00:33:43 -0500

    Search stuff.

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -3,9 +3,13 @@
 angular.module('sgmobileApp')
   .controller('SearchCtrl', function ($scope, Seatgeek) {
     $scope.getSuggestions = function() {
-      if ($scope.searchText && $scope.searchText.length > 2)
+      if ($scope.searchText && $scope.searchText.length > 2) {
         Seatgeek.Autocomplete.getSuggestions({q: $scope.searchText}, function(data) {
+          console.log(data);
           $scope.searchResults = data;
         });
+      } else {
+        $scope.searchResults = {};
+      }
     }
   });
